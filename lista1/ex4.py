c94ecd91b79a2b30088ca72f22986477c763637f
Eduardo Gondo <egondo@gmail.com>
2024-03-08 20:25:21 -0300

    Correções dos exercícios 4 e 7

---
+++ lista1/ex4.py
@@ -0,0 +1,12 @@
+numA = int(input("Digite um número"))
+numB = int(input("Digite outro número: "))
+
+soma = numA + numB
+mult = numA * numB
+divisao = numA // numB
+resto = numA % numB
+
+print("Soma", soma)
+print("Multiplicação", mult)
+print("Divisão inteira", divisao)
+print("Resto da divisão", resto)
---
+++ lista1/ex7.py
@@ -0,0 +1,8 @@
+aux = input("Informe o numero entre 0 e 99: ")
+num = int(aux)
+
+dezena = num // 10
+unidade = num % 10
+
+print("Dezena: ", dezena)
+print("Unidade: ", unidade)
