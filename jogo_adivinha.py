73ba44db4f3f722a5ecd894c604da102f3893a14
Eduardo Gondo <egondo@gmail.com>
2024-03-25 22:17:39 -0300

    Jogo do Adivinha

--- jogo_adivinha.py
+++ jogo_adivinha.py
@@ -6,4 +6,84 @@
 import random
 
 sorteado = random.randint(1, 1000)
-print(sorteado)
+#print(sorteado)
+
+chute = int(input("Tente adivinhar o número sorteado: "))
+if chute > sorteado:
+    print("Tente um número menor!")
+elif chute < sorteado:
+    print("Tente um número maior!")
+else:
+    print("Você acertou na 1 tentativa")
+
+chute = int(input("Tente adivinhar o número sorteado: "))
+if chute > sorteado:
+    print("Tente um número menor!")
+elif chute < sorteado:
+    print("Tente um número maior!")
+else:
+    print("Você acertou na 2 tentativa")
+
+chute = int(input("Tente adivinhar o número sorteado: "))
+if chute > sorteado:
+    print("Tente um número menor!")
+elif chute < sorteado:
+    print("Tente um número maior!")
+else:
+    print("Você acertou na 3 tentativa")
+
+chute = int(input("Tente adivinhar o número sorteado: "))
+if chute > sorteado:
+    print("Tente um número menor!")
+elif chute < sorteado:
+    print("Tente um número maior!")
+else:
+    print("Você acertou na 4 tentativa")
+
+chute = int(input("Tente adivinhar o número sorteado: "))
+if chute > sorteado:
+    print("Tente um número menor!")
+elif chute < sorteado:
+    print("Tente um número maior!")
+else:
+    print("Você acertou na 5 tentativa")
+
+chute = int(input("Tente adivinhar o número sorteado: "))
+if chute > sorteado:
+    print("Tente um número menor!")
+elif chute < sorteado:
+    print("Tente um número maior!")
+else:
+    print("Você acertou na 6 tentativa")
+
+chute = int(input("Tente adivinhar o número sorteado: "))
+if chute > sorteado:
+    print("Tente um número menor!")
+elif chute < sorteado:
+    print("Tente um número maior!")
+else:
+    print("Você acertou na 7 tentativa")
+
+chute = int(input("Tente adivinhar o número sorteado: "))
+if chute > sorteado:
+    print("Tente um número menor!")
+elif chute < sorteado:
+    print("Tente um número maior!")
+else:
+    print("Você acertou na 8 tentativa")        
+
+chute = int(input("Tente adivinhar o número sorteado: "))
+if chute > sorteado:
+    print("Tente um número menor!")
+elif chute < sorteado:
+    print("Tente um número maior!")
+else:
+    print("Você acertou na 9 tentativa")    
+
+chute = int(input("Tente adivinhar o número sorteado: "))
+if chute > sorteado:
+    print("Tente um número menor!")
+elif chute < sorteado:
+    print("Tente um número maior!")
+else:
+    print("Você acertou na 10 tentativa")    
