ec345103383600a47c4742269a236e12d87c8ac1
Logon PF Fiap <logonpflocal@fiap.com.br>
2024-05-03 21:09:58 -0300

    Exemplos de listas

---
+++ exemplo_funcao_objetos.py
@@ -0,0 +1,10 @@
+def caixa_alta(texto):
+    texto = texto.upper()
+    return texto
+
+
+frase = "Antes de ontem foi feriado!"
+nova_frase = caixa_alta(frase)
+print(nova_frase)
+
+frase = "Amanha é sábado!"
---
+++ exemplo_listas.py
@@ -0,0 +1,13 @@
+garagem = []
+
+garagem.append("Opala")
+garagem.append("Fusca")
+garagem.append("Mobi")
+garagem.append("Up")
+
+print(garagem)
+garagem.insert(3, "Fit")
+print(garagem)
+
+ferro_velho = garagem.pop(2)
+print(garagem)
