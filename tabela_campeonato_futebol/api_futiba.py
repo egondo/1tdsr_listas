10808c038ad1b97d4316841d410e175c722d27f7
Eduardo Gondo <egondo@gmail.com>
2024-10-17 07:53:55 -0300

    API

--- tabela_campeonato_futebol/api_futiba.py
+++ tabela_campeonato_futebol/api_futiba.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import negocio
+import traceback
 
 
 app = Flask(__name__)
 
+CORS(app, origins="http://127.0.0.1:5000")
+
 
 @app.route("/partidas", methods=["POST"])
+@cross_origin()
 def cadastra_partida():
     dado = request.json
     print(dado)
@@ -13,9 +18,11 @@ def cadastra_partida():
         negocio.cadastra_partida(dado)    
         return dado, 201
     except:
+        traceback.print_exc()
         return {'title': 'erro no cadastra partida', "status": 402}, 402
 
 @app.route("/times", methods=["GET"])
+@cross_origin()
 def recupera_times():
     try:
         dados = negocio.monta_tabela_classificacao()
@@ -29,15 +36,7 @@ def recupera_times():
             result.append(d)
         return result, 200
     except:
+        traceback.print_exc()
         return {'title': 'erro na recuperacao', 'status': 401}, 401
-
-
-
-
-
-
-
-
-
 
 app.run(debug=True)
--- tabela_campeonato_futebol/script.js
+++ tabela_campeonato_futebol/script.js
@@ -1,8 +1,40 @@
 const botao = document.querySelector("#botao")
+const cad = document.querySelector("#cadastra")
+
+cad.addEventListener("click", () => {
+    casa = document.querySelector('[name="casa"]')
+    pl_casa = document.querySelector('[name="placar_casa"]')
+    visi = document.querySelector('[name="visi"]')
+    pl_visi = document.querySelector('[name="placar_visi"]')
+    json_dado = {casa: casa.value, visi: visi.value, gc: parseInt(pl_casa.value), gv: parseInt(pl_visi.value), rodada: 3}
+
+    fetch("http://127.0.0.1:5000/partidas", 
+        {
+            method: "POST",
+            body: JSON.stringify(json_dado),
+            headers: {"Content-Type": "application/json"}
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+})
+
 
 botao.addEventListener("click", () => {
     console.log("teste")
-    fetch("http://127.0.0.1:5000/times").
-        then(json => json.data).
-        then(console.log(json))
+    fetch("http://127.0.0.1:5000/times")
+        .then(response => response.json())
+        .then(data => {
+            dados = JSON.stringify(data)
+            alert(dados)
+            tab = "<table>"
+            data.forEach(function(element){
+                //console.log(element.nome);
+                //alert(element.nome)
+                tab = tab + "<tr><td>" + element.nome + "<td>" + element.pontos + "<td>" + element.jogos 
+            });
+            tab = tab + "</table>"
+
+            div = document.querySelector("#tab")
+            div.innerHTML = tab
+        })
 })
